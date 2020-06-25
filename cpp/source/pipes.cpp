637d082d7d66bbe3c588c2b0641d4fc3b1366391
Spencer Goodrich <goodrichse@gmail.com>
2020-06-25 10:11:13 -0700

    Finally got packages linked correctly. Still working on getting a camera feed sent to a window.

--- cpp/source/pipes.cpp
+++ cpp/source/pipes.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<jetson-utils/commandLine.h>
 #include<jetson-utils/gstCamera.h>
+#include<jetson-utils/glDisplay.h>
 //#include"boilerplate.h"
 #include"TestClass.h"
 //#include"start.h"
@@ -9,6 +10,17 @@ using namespace std;
 
 int usage(){
 
+	printf("Improper format. Usage:\n");
+	printf("pipes [--network NETWORK] [--camera CAMERA]\n");
+        printf("	[--width WIDTH] [--height HEIGHT]\n\n");
+	printf("Run an image processing pipeline on a camera stream.\n\n");
+	printf("Options:");
+	printf("	--help			Show this message.\n");
+	printf("	--network 	NETWORK	Pre-trained network to load.\n");
+	printf("	--camera 	CAMERA	e.g. for VL42 cameras, the /dev/video device to use.\n");
+	printf("	--width		INT	Width of stream (default 1280).\n");
+	printf("	--height	INT	Height of stream (default 720).\n");
+	printf("");
 	return 0;
 }
 
@@ -21,8 +33,10 @@ int main(int argc, char** argv){
 
 	char* nptr = 0;
 
+	// Testing
 	printf("entering main\n");
 
+	// Create camera object
         gstCamera* camera = gstCamera::Create(
                         cmdl.GetInt("width", gstCamera::DefaultWidth),
                         cmdl.GetInt("height", gstCamera::DefaultHeight),
@@ -30,28 +44,31 @@ int main(int argc, char** argv){
 
         if(!camera) 
         { 
-                printf("TestClass: failed to initialize camera.\n"); 
+                printf("Pipes: failed to initialize camera.\n"); 
                 return -1; 
         } 
  
-        printf("TestClass:  successfully initialized camera.\n"); 
+	// Testing
+        printf("Pipes:  successfully initialized camera.\n"); 
         printf("    width:  %u\n", camera->GetWidth()); 
         printf("   height:  %u\n", camera->GetHeight()); 
         printf("    depth:  %u (bpp)\n\n", camera->GetPixelDepth()); 
  
- 
+	// Create display window
+	glDisplay* display = glDisplay::Create();
+
+	// Open camera
         if(!camera->Open()) 
         { 
-                printf("TestClass: camera failed to open.\n"); 
+                printf("Pipes: camera failed to open.\n"); 
                 return -2; 
         } 
-	
 
-//	TestClass feed = TestClass();
-	TestClass feed;
-//	TestClass feed();
-	feed.testCall();
-//	bool success = feed.testWindow();
+//	TestClass feed;
+//	feed.testCall();
+
+	SAFE_DELETE(camera);
+	SAFE_DELETE(display);
 
 	return 0;
 }
